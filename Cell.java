418110ab16b78441c0c7551e29ced407c7bc181c
Geena Kim <gkim90@stuy.edu>
2019-01-04 14:33:08 -0500

    yesterday's devLog and only cell class

--- Cell.java
+++ Cell.java
@@ -2,4 +2,30 @@ public class Cell{
   private int coorX;
   private int coorY;
 
+
+
+
+
 }
+
+
+
+/*
+public class Mine{
+  //fields
+  public int coorx;
+  public int coory;
+  public int numMines;
+
+  //constructor
+  //this is the default constructor that creates ten mines no matter how many cells are instantiated
+  public Mine(){
+    numMines = 10;
+    //can't put randomizer here because cell is Mine's child
+  }
+
+  //swap IndexOut
+
+  //
+}
+*/
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MKS21X-FinalProject
 Team Gecko's Amazing Minesweeper !!!
+
+1/3/18
+We began writing up the class Mine and Cell (constructors and headers). However, after reviewing our class structure, decided to make classes Cell and Board.
