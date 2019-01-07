fb2f363e6ed9eaf6cc5117edb8250c3cc90f219b
Geena Kim <gkim90@stuy.edu>
2019-01-07 14:48:42 -0500

    clear in Board class

--- Board.java
+++ Board.java
@@ -7,6 +7,16 @@ public class Board{
   //this is the default grid which instatiates a 10 by 10 grid and plants 10 random mines
   public Board(){
     cellGrid = new Cell[10][10];
-
+    clear();
   }
+
+  private void clear(){
+    for (int x = 0; x < cellGrid.length; x++){
+      for (int y = 0; y < cellGrid[x].length; y++){
+        cellGrid[x][y] = new Cell();
+      }
+    }
+  }
+
+
 }
