cd0ed9851030a7907d93c194e1e6c79682823b66
Geena Kim <gkim90@stuy.edu>
2019-01-07 14:42:50 -0500

    all constructors

--- Cell.java
+++ Cell.java
@@ -1,15 +1,32 @@
 public class Cell{
   public boolean mine;// false means no mine; true means cell has mine
   public int coveredStatus;// positive means uncovered; 0 means covered; negative means flagged
-  public int  mineNum;
+  public int  mineNum;//the number of neighboring cells with mines; if cell has a mine, this number is negative
+  public int coorX;
+  public int coorY;
 
 
   //constructors
-  //default constructor: sets
+  //default constructor: covered cell with no mine, no neighbor mines at (0,0)
   public Cell(){
     mine = false;
     coveredStatus = 0;
+    mineNum = 0;
+    coorX = 0;
+    coorY = 0;
   }
+
+  public Cell(boolean aMine, int coveredStat, int mineNumber, int theX, int theY){
+    mine = aMine;
+    coveredStatus = coveredStat;
+    coorX = theX;
+    coorY = theY;
+    if(mine == true){
+      mineNum = (mineNum * 0) - 15; //mine cells have mineNum -15;
+    }
+  }
+
+
 
   //methods start here
   public boolean hasMine(){
