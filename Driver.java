85e38bb55a5f9839e97ef01421368b56485ab45d
Geena Kim <gkim90@stuy.edu>
2019-01-11 01:39:35 -0500

    try catch statement in driver started

--- Driver.java
+++ Driver.java
@@ -3,15 +3,27 @@ import java.io.*; //file, filenotfoundexception
 public class Driver{
   public static void main(String[] args) {
     //Board(int rows, int cols, int mines, int seeder): constructor for reference
-    //rows & cols
+    //rows & cols & mines
     int defaultRow = 10;
     int defaultCol = 10;
+    int defaultMines = 10;
     //randSeed
     Random randooo = new Random();
     int seed = randooo.nextInt(1001);
 
+    String instruction = "ENTER INSTRUCTION HERE L8TER";
 
+    try{
     //if number of mines > number of cells existing, print error
+    if((Integer.parseInt(args[0]) < 0) || (Integer.parseInt(args[1]) < 0)){
+        System.out.println("The row and column must be positive" + '\n' + instruction);
+        System.exit(1);
+      }
+    }
+    catch(Exception e){
+      System.out.println("You messed up somewhere hun. Check yo self" + '\n' + instruction);
+    }
+
 
 
   /*  Board tester = new Board();
