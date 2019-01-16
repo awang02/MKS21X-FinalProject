1f22fafba0a544679040171d414e061bb0841d46
Geena Kim <gkim90@stuy.edu>
2019-01-16 02:03:30 -0500

    combined Driver and TerminalBoard onto Driver

--- Driver.java
+++ Driver.java
@@ -21,6 +21,12 @@ import java.util.*; //random, scanner, arraylist
 import java.io.*; //file, filenotfoundexception
 
 public class Driver{
+  public static void putString(int r, int c,Terminal t, String s){
+		t.moveCursor(r,c);
+		for(int i = 0; i < s.length();i++){
+			t.putCharacter(s.charAt(i));
+		}
+	}
   public static void main(String[] args) {
     //Board(int rows, int cols, int mines, int seeder): constructor for reference
     //rows & cols & mines
@@ -87,6 +93,114 @@ public class Driver{
     Board tester6 = new Board(6, 6, 10, 2);
     System.out.println(tester6);
     //*/
+
+
+
+
+    		int x = 10;
+    		int y = 10;
+
+    		Terminal terminal = TerminalFacade.createTextTerminal();
+    		terminal.enterPrivateMode();
+
+    		TerminalSize size = terminal.getTerminalSize();
+    		terminal.setCursorVisible(false);
+
+    		boolean running = true;
+
+    		long tStart = System.currentTimeMillis();
+    		long lastSecond = 0;
+
+    		while(running){
+
+    			terminal.moveCursor(x,y);
+    			terminal.applyBackgroundColor(Terminal.Color.WHITE);
+    			terminal.applyForegroundColor(Terminal.Color.BLACK);
+    			//applySGR(a,b) for multiple modifiers (bold,blink) etc.
+    			terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
+    			terminal.putCharacter('\u00a4');
+    			//terminal.putCharacter(' ');
+    			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+    			terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+    			terminal.applySGR(Terminal.SGR.RESET_ALL);
+
+
+    			terminal.moveCursor(size.getColumns()-5,5);
+    	//		terminal.applyBackgroundColor(Terminal.Color.BLUE);
+    	//		terminal.applyForegroundColor(Terminal.Color.WHITE);
+    			terminal.applySGR(Terminal.SGR.ENTER_BOLD);
+    			terminal.putCharacter(' ');
+    			terminal.putCharacter(' ');
+    	//		terminal.putCharacter('\u262d');
+    			terminal.putCharacter(' ');
+    			terminal.moveCursor(size.getColumns()-5,6);
+    			terminal.putCharacter(' ');
+    			terminal.putCharacter(' ');
+    			terminal.putCharacter(' ');
+    			terminal.putCharacter(' ');
+    	//		terminal.applyBackgroundColor(Terminal.Color.DEFAULT);// highlight
+    	//		terminal.applyForegroundColor(Terminal.Color.DEFAULT);//words
+
+
+//LANTERNA STUFFS
+    			Key key = terminal.readInput();
+
+    			if (key != null)
+    			{
+
+    				if (key.getKind() == Key.Kind.Escape) {
+
+    					terminal.exitPrivateMode();
+    					running = false;
+    				}
+
+    				if (key.getKind() == Key.Kind.ArrowLeft) {
+    					terminal.moveCursor(x,y);
+    					terminal.putCharacter(' ');
+    					x--;
+    				}
+
+    				if (key.getKind() == Key.Kind.ArrowRight) {
+    					terminal.moveCursor(x,y);
+    					terminal.putCharacter(' ');
+    					x++;
+    				}
+
+    				if (key.getKind() == Key.Kind.ArrowUp) {
+    					terminal.moveCursor(x,y);
+    					terminal.putCharacter(' ');
+    					y--;
+    				}
+
+    				if (key.getKind() == Key.Kind.ArrowDown) {
+    					terminal.moveCursor(x,y);
+    					terminal.putCharacter(' ');
+    					y++;
+    				}
+    				//space moves it diagonally
+    				if (key.getCharacter() == ' ') {
+    					terminal.moveCursor(x,y);
+    					terminal.putCharacter(' ');
+    					y++;
+    					x++;
+    				}
+    				putString(1,4,terminal,"["+key.getCharacter() +"]");
+    				putString(1,1,terminal,key+"        ");//to clear leftover letters pad withspaces
+    			}
+
+    			//DO EVEN WHEN NO KEY PRESSED:
+    			long tEnd = System.currentTimeMillis();
+    			long millis = tEnd - tStart;
+    			putString(1,2,terminal,"Milliseconds since start of program: "+millis);
+    			if(millis/1000 > lastSecond){
+    				lastSecond = millis / 1000;
+    				//one second has passed.
+    				putString(1,3,terminal,"Seconds since start of program: "+lastSecond);
+
+    			}
+
+
+    		}
   }
 
 
