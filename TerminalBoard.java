32a4dc12207f3f3dea93c16d4fc9eb0d21f10cc4
Geena Kim <gkim90@stuy.edu>
2019-01-16 00:53:28 -0500

    working backwards from what's given in TerminalDemo for TerminalBoard

--- TerminalBoard.java
+++ TerminalBoard.java
@@ -26,69 +26,110 @@ public class TerminalBoard{
 		}
 	}
 
-	public static void main(String[] args){
+  public static void main(String[] args) {
 
-  	int x = 10;
-  	int y = 10;
 
-    Terminal terminal = TerminalFacade.createTextTerminal();
-    terminal.enterPrivateMode();
+		int x = 10;
+		int y = 10;
 
-  	TerminalSize size = terminal.getTerminalSize();
-  	terminal.setCursorVisible(false);
+		Terminal terminal = TerminalFacade.createTextTerminal();
+		terminal.enterPrivateMode();
 
-    boolean running = true;
+		TerminalSize size = terminal.getTerminalSize();
+		terminal.setCursorVisible(false);
+
+		boolean running = true;
 
 		long tStart = System.currentTimeMillis();
 		long lastSecond = 0;
 
+		while(running){
 
-    Key key = terminal.readInput();
-
-    if (key != null)
-    {
-
-      if (key.getKind() == Key.Kind.Escape) {
-
-        terminal.exitPrivateMode();
-        running = false;
-      }
-
-      if (key.getKind() == Key.Kind.ArrowLeft) {
-        terminal.moveCursor(x,y);
-        terminal.putCharacter(' ');
-        x--;
-      }
-
-      if (key.getKind() == Key.Kind.ArrowRight) {
-        terminal.moveCursor(x,y);
-        terminal.putCharacter(' ');
-        x++;
-      }
-
-      if (key.getKind() == Key.Kind.ArrowUp) {
-        terminal.moveCursor(x,y);
-        terminal.putCharacter(' ');
-        y--;
-      }
-
-      if (key.getKind() == Key.Kind.ArrowDown) {
-        terminal.moveCursor(x,y);
-        terminal.putCharacter(' ');
-        y++;
-      }
-      //space moves it diagonally
-      if (key.getCharacter() == ' ') {
-        terminal.moveCursor(x,y);
-        terminal.putCharacter(' ');
-        y++;
-        x++;
-      }
-      putString(1,4,terminal,"["+key.getCharacter() +"]");
-      putString(1,1,terminal,key+"        ");//to clear leftover letters pad withspaces
-    }
+			terminal.moveCursor(x,y);
+			terminal.applyBackgroundColor(Terminal.Color.WHITE);
+			terminal.applyForegroundColor(Terminal.Color.BLACK);
+			//applySGR(a,b) for multiple modifiers (bold,blink) etc.
+			terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
+			terminal.putCharacter('\u00a4');
+			//terminal.putCharacter(' ');
+			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+			terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+			terminal.applySGR(Terminal.SGR.RESET_ALL);
 
 
-  }
+			terminal.moveCursor(size.getColumns()-5,5);
+			terminal.applyBackgroundColor(Terminal.Color.BLUE);
+			terminal.applyForegroundColor(Terminal.Color.WHITE);
+			terminal.applySGR(Terminal.SGR.ENTER_BOLD);
+			terminal.putCharacter(' ');
+			terminal.putCharacter(' ');
+			terminal.putCharacter('\u262d');
+			terminal.putCharacter(' ');
+			terminal.moveCursor(size.getColumns()-5,6);
+			terminal.putCharacter(' ');
+			terminal.putCharacter(' ');
+			terminal.putCharacter(' ');
+			terminal.putCharacter(' ');
+			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);// highlight
+			terminal.applyForegroundColor(Terminal.Color.DEFAULT);//words
 
+			Key key = terminal.readInput();
+
+			if (key != null)
+			{
+
+				if (key.getKind() == Key.Kind.Escape) {
+
+					terminal.exitPrivateMode();
+					running = false;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowLeft) {
+					terminal.moveCursor(x,y);
+					terminal.putCharacter(' ');
+					x--;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowRight) {
+					terminal.moveCursor(x,y);
+					terminal.putCharacter(' ');
+					x++;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowUp) {
+					terminal.moveCursor(x,y);
+					terminal.putCharacter(' ');
+					y--;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowDown) {
+					terminal.moveCursor(x,y);
+					terminal.putCharacter(' ');
+					y++;
+				}
+				//space moves it diagonally
+				if (key.getCharacter() == ' ') {
+					terminal.moveCursor(x,y);
+					terminal.putCharacter(' ');
+					y++;
+					x++;
+				}
+				putString(1,4,terminal,"["+key.getCharacter() +"]");
+				putString(1,1,terminal,key+"        ");//to clear leftover letters pad withspaces
+			}
+
+			//DO EVEN WHEN NO KEY PRESSED:
+			long tEnd = System.currentTimeMillis();
+			long millis = tEnd - tStart;
+			putString(1,2,terminal,"Milliseconds since start of program: "+millis);
+			if(millis/1000 > lastSecond){
+				lastSecond = millis / 1000;
+				//one second has passed.
+				putString(1,3,terminal,"Seconds since start of program: "+lastSecond);
+
+			}
+
+
+		}
+	}
 }
