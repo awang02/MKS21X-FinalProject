b39e17cdcb638dc7ddd662b4d09fa38016a62987
Geena Kim <gkim90@stuy.edu>
2019-01-18 05:43:36 -0500

    back to printing

--- Driver.java
+++ Driver.java
@@ -23,8 +23,91 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Driver{
+  public static void main(String[] args){
+    //USER INPUT SYSTEM BEING WORKED ON FOR LATERS
+      //Board(int rows, int cols, int mines, int seeder): constructor for reference
+      //rows & cols & mines
+      int defaultRow = 10;
+      int defaultCol = 10;
+      int defaultMines = 10;
+      //randSeed
+      Random randooo = new Random();
+      int seed = randooo.nextInt(1001);
 
-  public static void putString(int r, int c,Terminal t, String s){
+      //CREATE BETTER INSTRUCTIONS L8ER
+      String instruction = "Enter number of rows, number of columns and number of mines. optional: enter seed num";
+
+      try{
+
+      //if number of mines > number of cells existing, print error
+        if((Integer.parseInt(args[0]) < 0) || (Integer.parseInt(args[1]) < 0)){
+          System.out.println("The row and column must be positive" + '\n' + instruction);
+          System.exit(1);
+        }
+        if (args.length > 2 && Integer.parseInt(args[0]) * Integer.parseInt(args[1]) < Integer.parseInt(args[2])){
+          System.out.println("The number of mines exceeds the number of cells" + '\n' + instruction);
+          System.exit(1);
+        }
+        if (args.length < 3){
+          System.out.println(instruction);
+        }
+        if (args.length == 3){
+          defaultRow = Integer.parseInt(args[0]);
+          defaultCol = Integer.parseInt(args[1]);
+          defaultMines = Integer.parseInt(args[2]);
+          System.out.println(new Board(defaultRow, defaultCol, defaultMines, seed));
+        }
+        if (args.length >= 4 && (Integer.parseInt(args[4]) < 0 || Integer.parseInt(args[4]) > 10000 )){
+          System.out.println("The seed (fourth argument) must be between 0 and 10000 inclusive" + '\n' + instruction);
+          System.exit(1);
+        }
+        if (args.length == 4 || args.length > 4){
+          defaultRow = Integer.parseInt(args[0]);
+          defaultCol = Integer.parseInt(args[1]);
+          defaultMines = Integer.parseInt(args[2]);
+          seed = Integer.parseInt(args[3]);
+          System.out.println(new Board(defaultRow, defaultCol, defaultMines, seed));
+        }
+      }
+      catch(Exception e){
+        System.out.println("You messed up somewhere hun. Check yo self" + '\n' + instruction);
+      }
+
+
+
+  //    Board tester = new Board();
+  //    System.out.println(tester);
+      Board tester1 = new Board(15, 15, 15, 37253);
+      System.out.println(tester1.toStringBoard);
+  //    Board tester2 = new Board(15, 15, 10, 67223);
+  //    System.out.println(tester2);
+  //    Board tester3 = new Board(15, 15, 10, 223);
+  //    System.out.println(tester3);
+  //    Board tester4 = new Board(15, 15, 10, 3723);
+  //    System.out.println(tester4);
+  //    Board tester5 = new Board(15, 15, 10, 3723); //CRASHES WHYYY
+  //    System.out.println(tester5);
+  //    Board tester6 = new Board(6, 6, 10, 2);
+  //    System.out.println(tester6);
+      //
+  }
+  public String toStringBoard(){
+    // the numbers in the for loops have been modefied to only show the main cells and none of the buffer
+    String print = "|";
+    for (int t = 1; t < cellGrid.length - 1; t++){
+      print += "|";
+      for (int y = 1; y < cellGrid[t].length - 2; y++){
+        print += "|" + " " + " ";
+      }
+      print += cellGrid[t][cellGrid[t].length - 2];
+      print += "|\n";
+    }
+    return print + "\nSeed: " + seed;
+  }
+
+}
+
+/*  public static void putString(int r, int c,Terminal t, String s){
 		t.moveCursor(r,c);
 		for(int i = 0; i < s.length();i++){
 			t.putCharacter(s.charAt(i));
@@ -75,7 +158,7 @@ public class Driver{
       	terminal.applyBackgroundColor(Terminal.Color.BLACK);
       //	terminal.applyForegroundColor(Terminal.Color.WHITE);
         terminal.applySGR(Terminal.SGR.ENTER_BOLD);
-
+        for()
         terminal.putCharacter(' ');
         terminal.putCharacter(' ');
         terminal.putCharacter(' ');
@@ -84,7 +167,17 @@ public class Driver{
       	terminal.applyBackgroundColor(Terminal.Color.DEFAULT);// highlight
   		  terminal.applyForegroundColor(Terminal.Color.DEFAULT);//words
 
-
+        terminal.moveCursor(size.getColumns()-100,10);
+        terminal.applyBackgroundColor(Terminal.Color.BLACK);
+        //	terminal.applyForegroundColor(Terminal.Color.WHITE);
+        terminal.applySGR(Terminal.SGR.ENTER_BOLD);
+        terminal.putCharacter(' ');
+        terminal.putCharacter(' ');
+        terminal.putCharacter(' ');
+        terminal.putCharacter(' ');
+        terminal.putCharacter(' ');
+        terminal.applyBackgroundColor(Terminal.Color.DEFAULT);// highlight
+        terminal.applyForegroundColor(Terminal.Color.DEFAULT);//words
 
 
 //LANTERNA STUFFS
@@ -158,3 +251,4 @@ public class Driver{
   	}
 	}
 }
+*/
