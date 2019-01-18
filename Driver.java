24c8dc05e5115aa7b0bdd635195ba686738b7d6c
Geena Kim <gkim90@stuy.edu>
2019-01-18 12:16:22 -0500

    driver board written up

--- Driver.java
+++ Driver.java
@@ -38,35 +38,50 @@ public class Driver{
       String instruction = "Enter number of rows, number of columns and number of mines. optional: enter seed num";
 
       try{
-
       //if number of mines > number of cells existing, print error
         if((Integer.parseInt(args[0]) < 0) || (Integer.parseInt(args[1]) < 0)){
           System.out.println("The row and column must be positive" + '\n' + instruction);
           System.exit(1);
         }
-        if (args.length > 2 && Integer.parseInt(args[0]) * Integer.parseInt(args[1]) < Integer.parseInt(args[2])){
+        else if (args.length > 2 && Integer.parseInt(args[0]) * Integer.parseInt(args[1]) < Integer.parseInt(args[2])){
           System.out.println("The number of mines exceeds the number of cells" + '\n' + instruction);
           System.exit(1);
         }
-        if (args.length < 3){
+        else if (args.length < 3){
           System.out.println(instruction);
         }
-        if (args.length == 3){
-          defaultRow = Integer.parseInt(args[0]);
-          defaultCol = Integer.parseInt(args[1]);
+        else if (args.length == 3){
+          defaultRow = Integer.parseInt(args[0]) + 2;
+          defaultCol = Integer.parseInt(args[1]) + 2;
           defaultMines = Integer.parseInt(args[2]);
-          System.out.println(new Board(defaultRow, defaultCol, defaultMines, seed));
+          Board B = new Board(defaultRow, defaultCol, defaultMines, seed);
+          System.out.println(B.toStringBoard());
         }
-        if (args.length >= 4 && (Integer.parseInt(args[4]) < 0 || Integer.parseInt(args[4]) > 10000 )){
+        else if (args.length >= 4 && (Integer.parseInt(args[4]) < 0 || Integer.parseInt(args[4]) > 10000 )){
           System.out.println("The seed (fourth argument) must be between 0 and 10000 inclusive" + '\n' + instruction);
           System.exit(1);
         }
-        if (args.length == 4 || args.length > 4){
+        else if (args.length == 4 || args.length > 4){
           defaultRow = Integer.parseInt(args[0]);
           defaultCol = Integer.parseInt(args[1]);
           defaultMines = Integer.parseInt(args[2]);
           seed = Integer.parseInt(args[3]);
-          System.out.println(new Board(defaultRow, defaultCol, defaultMines, seed));
+          Board B = new Board(defaultRow, defaultCol, defaultMines, seed);
+          System.out.println(B.toStringBoard());
+        }
+        else if (args.length == 5 || args.length > 5){
+          defaultRow = Integer.parseInt(args[0]);
+          defaultCol = Integer.parseInt(args[1]);
+          defaultMines = Integer.parseInt(args[2]);
+          seed = Integer.parseInt(args[3]);
+          Board B = new Board(defaultRow, defaultCol, defaultMines, seed);
+          if(args[5].equals("key")){
+            System.out.println(B.toStringBoard());
+          }
+          else{
+            System.out.println(B);
+
+          }
         }
       }
       catch(Exception e){
@@ -77,9 +92,9 @@ public class Driver{
 
   //    Board tester = new Board();
   //    System.out.println(tester);
-      Board tester1 = new Board(15 + 2, 15 + 2, 15, 37253);
-      System.out.println(tester1.toStringBoard());
-      System.out.println(tester1);
+//      Board tester1 = new Board(15 + 2, 15 + 2, 15, 37253);
+//      System.out.println(tester1.toStringBoard());
+//      System.out.println(tester1);
   //    Board tester2 = new Board(15, 15, 10, 67223);
   //    System.out.println(tester2);
   //    Board tester3 = new Board(15, 15, 10, 223);
